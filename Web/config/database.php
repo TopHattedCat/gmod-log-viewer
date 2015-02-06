31ca297fce7930047c5d0e47e3b3e9f944375963
Sam Simpson <samsamsimpson02@gmail.com>
2015-02-06 13:25:09 +0000

    Update database.php

--- Web/config/database.php
+++ Web/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'logs',
 			'username'  => 'root',
-			'password'  => 'Jaminaplantpot01',
+			'password'  => 'yourpassword',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
